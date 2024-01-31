9196b3ae53d16af6868faae4e81a1d78d95a7a42
igurvir <sin12634@sheridancollege.ca>
2024-01-31 11:08:03 -0500

    commit first

--- src/card/CardTrick.java
+++ src/card/CardTrick.java
@@ -3,7 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package card;
-
+import java.util.Scanner;
 /**
  * A class that fills a magic hand of 7 cards with random Card Objects
  * and then asks the user to pick a card and searches the array of cards
@@ -16,17 +16,50 @@ public class CardTrick {
     {
         Card[] magicHand = new Card[7];
         
-        for (int i=0; i<magicHand.length; i++)
-        {
+        for (int i = 0; i < magicHand.length; i++) {
             Card c = new Card();
-            //c.setValue(insert call to random number generator here)
-            //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
+            c.setValue(generateRandomValue());
+            c.setSuit(Card.SUITS[generateRandomNumber(0, 3)]);
+            magicHand[i] = c;
         }
+        
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter your card value: ");
+        int userValue = scanner.nextInt();
+        System.out.print("Enter your card suit (0-3): ");
+        int userSuitIndex = scanner.nextInt();
+        
+        Card userCard = new Card();
+        userCard.setValue(userValue);
+        userCard.setSuit(Card.SUITS[userSuitIndex]);
+        
+          boolean foundMatch = false;
+        for (Card card : magicHand) {
+            if (card.equals(userCard)) {
+                foundMatch = true;
+                break;
+            }
+        }
+        
+          if (foundMatch) {
+            System.out.println("Congratulations! You found a match in the magic hand.");
+        } else {
+            System.out.println("Sorry, no match found in the magic hand.");
+        }
+    
         
         //insert code to ask the user for Card value and suit, create their card
         // and search magicHand here
         //Then report the result here
         // add one luckcard hard code 2,clubs
     }
+     private static int generateRandomNumber(int min, int max) {
+        return (int) (Math.random() * (max - min + 1) + min);
+    }
+     
+      private static int generateRandomValue() {
+        return generateRandomNumber(1, 13);
+    }
+
     
 }
