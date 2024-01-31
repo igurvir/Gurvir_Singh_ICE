ba314a6264afc9f983254b0e237e792ecec2dad5
igurvir <sin12634@sheridancollege.ca>
2024-01-31 14:57:30 -0500

    Update CardTrick.java
    adding hard coded luckyCard

--- src/card/CardTrick.java
+++ src/card/CardTrick.java
@@ -24,6 +24,16 @@ public class CardTrick {
             c.setSuit(Card.SUITS[(int) (Math.random() * 4)]);
             magicHand[i] = c;
         }
+        //adding hardcored luckyCard
+        Card luckyCard = new Card();
+        luckyCard.setValue(2);
+        luckyCard.setSuit("Clubs");
+        magicHand[3] = luckyCard; //replacing it for a card in the deck
+
+        // Print the magicHand
+        for (Card card : magicHand) {
+            System.out.println(card.getSuit() + " " + card.getValue());
+        }
 
       
 
